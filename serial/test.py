e19c41d0a8cf421895f9f3ccf56713cc3beda336
nam <namtrungnguyen299@gmail.com>
2019-10-02 15:08:42 +0700

    database connect

--- serial/test.py
+++ serial/test.py
@@ -1,5 +1,10 @@
 import serial 
 import mysql.connector
+import paho.mqtt.client as mqtt
+import datetime
+THINGSBOARD_HOST = 'demo.thingsboard.io'
+ACCESS_TOKEN = 'Y3wJ8SUkSDe2CIdXKWdu'
+#database configure
 mydb = mysql.connector.connect(
    host="us-cdbr-iron-east-02.cleardb.net",
    user="b801f230629d30",
@@ -8,6 +13,12 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
+#thingsboard configure
+client - mqtt.Client()
+client.username_pw_set(ACCESS_TOKEN)
+client.connect(THINGSBOARD_HOST, 1883, 60)
+client.loop_start()
+#USB configure
 port = "/dev/ttyUSB0"
 serialfromarduino = serial.Serial(port,115200)  
 #serialfromarduino.flushInput()
@@ -25,6 +36,7 @@ humi = []
 ec = []
 ph = []
 lux = []
+status = {'status' : ''}
 while True:
  if(serialfromarduino.inWaiting()>0) : 
       a = serialfromarduino.readline() 
@@ -75,12 +87,20 @@ while True:
           print (lux)
           count=count+1
       if (count%6==0) : 
-         sql = "INSERT INTO data_nct (temp,humi,lux,pH,ec,water_temp) value (%s,%s,%s,%s,%s,%s)"
-         
-         val = (count_t,count_h,count_l,count_p,count_e,count_wt)
+         x = datetime.datetime.now()
+         sql = "INSERT INTO data_nct (temp,humi,lux,pH,ec,water_temp,timestamp) value (%s,%s,%s,%s,%s,%s,%s)"  
+         val = (count_t,count_h,count_l,count_p,count_e,count_wt,x)
          mycursor.execute(sql,val)
          mydb.commit()
+         if (count_t == 0 || count_h == 0 || count_l == 0 || count_p ==0 || count_e == 0 || count_wt == 0) :
+             status['status'] = 'Sensor is not active'
+             client.publish('v1/devices/me/telemetry',json.dumps(status),1)
+         else :
+             status['status'] = 'Sensor is active'
+             client.publish('v1/devices/me/telemetry',json.dumps(status),1)
       file.write(c)
       file.seek(0,2)
+client.loop_stop()
+client.disconnect()
 file.close()
 
